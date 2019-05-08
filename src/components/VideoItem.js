8a4d3922de0d170c10f2f577b077863042292a90
Riwandi <riwandy10@gmail.com>
2019-05-08 11:21:40 +0700

    adding video items

---
+++ src/components/VideoItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+
+const VideoItem = ({video}) => {
+
+
+  return (
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="images"/>
+        <div className="content">
+          <div className="header">
+            {video.snippet.title}
+          </div>
+        </div>
+    </div>
+  );
+};
+
+
+export default VideoItem;
