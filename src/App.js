e358eb35629b2d42dfe2632a1d57da7b7fa4adb4
Riwandi <riwandy10@gmail.com>
2019-05-08 11:21:22 +0700

    include data state videos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 import youtubedata from './APIs/youtubedata';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.SubmitInput} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
